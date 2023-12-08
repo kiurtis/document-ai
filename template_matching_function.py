6d56c12de01eb5e9e37135cffdb0e35bc356a8f1
Amiel Sitruk <amiel.sitruk@gmail.com>
2023-12-08 18:14:49 +0100

    feat(template_matching): pass device as an env variable

--- template_matching_function.py
+++ template_matching_function.py
@@ -810,7 +810,7 @@ else:
 sam = sam_model_registry[model_type](checkpoint=CHECKPOINT_PATH)
 
 #If cuda available uncomment that line
-sam.to(device=device)
+sam.to(device=DEVICE)
 
 mask_generator_2 = SamAutomaticMaskGenerator(
     model=sam,
